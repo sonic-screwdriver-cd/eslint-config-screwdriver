f667763569017572272b26436565f2bf36c3b43b
wdstar <wdstar@users.noreply.github.com>
2019-08-18 10:28:45 +0900

    feat(1650): add prettier.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
 
 module.exports = {
-    extends: 'airbnb-base',
+    extends: [
+        'airbnb-base',
+        'plugin:prettier/recommended' // last one wins
+    ],
     env: {
         browser: true,
         es6: true,
@@ -14,6 +17,7 @@ module.exports = {
     rules: {
         'comma-dangle': ['error', 'never'],
         'class-methods-use-this': 'off',
+        complexity: 'warn',
         eqeqeq: ['error', 'always'],
         'import/newline-after-import': ['off'],
         'import/no-extraneous-dependencies': ['error', {
@@ -23,6 +27,9 @@ module.exports = {
         }],
         indent: ['error', 4],
         'max-len': ['error', { code: 100, ignoreComments: true }],
+        'max-lines-per-function': 'warn',
+        'max-params': 'warn',
+        'max-statements': 'warn',
         'new-cap': ['error', { capIsNewExceptions: ['Given', 'When', 'Then', 'Before', 'After'] }],
         'newline-after-var': ['error', 'always'],
         'newline-before-return': 'error',
@@ -31,6 +38,13 @@ module.exports = {
         'no-param-reassign': ['error', { props: false }],
         'prefer-rest-params': 'off',
         'prefer-spread': 'off',
+        'prettier/prettier': [
+            'error',
+            {
+                singleQuote: true,
+                tabWidth: 4
+            }
+        ],
         'require-jsdoc': ['error', {
             require: {
                 FunctionDeclaration: true,
